21b1f14acd715b0c44a0b5ee33ea472949e60161
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-08 01:01:05 +0100

    first two checks of assignment one completed

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,5 +1,18 @@
 class ShoppingCart {
-  constructor()
+  constructor() {
+    this.content = [];
+  }
+  getItems() {
+    console.log(this.content);
+    return this.content;
+  }
+  addItem(itemName, quantity, price) {
+    this.content.push({
+      name: itemName,
+      quantity: quantity,
+      pricePerUnit: price
+    });
+  }
 }
 
 module.exports = ShoppingCart;
